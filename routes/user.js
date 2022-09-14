c91a4589fa2dcaaba0e6313450b42d4512c3f0f8
Kyle Christian <kylebolanos@gmail.com>
2022-09-13 17:33:52 -0700

    fixed bug preventing the server from starting

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const todosController = require('../controllers/todos')
 const userController = require('../controllers/user') 
 const { ensureAuth } = require('../middleware/auth')
 
-router.get('/', ensureAuth, userController.getStats)
+router.get('/', userController.getStats)
 
 module.exports = router
