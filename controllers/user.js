8efdf327377357cf67a44db8359a9c2884844a9b
Kyle Christian <kylebolanos@gmail.com>
2022-09-15 15:49:39 -0700

    cleaned up code and added new time: property to User model

--- controllers/user.js
+++ controllers/user.js
@@ -1,22 +1,12 @@
 const Stats = require('../models/Stats')
 
 module.exports = {
-    getStatsAlt: async (req, res) => {
+    getStats: async (req, res) => {
         console.log(req.user)
         try {
             const userTime = await Stats.find({userId:req.user.id})
             res.render('userPage.ejs', {time: userTime, user: req.user})
         }catch(err) {
-            console.log(err)
-        }
-    },
-    createStats: async (req,res) => {
-        console.log('hello')
-        try {
-            await Stats.create({name: req.user.userName, time: 0, userId: req.user.id})
-            console.log('Stat has been recorded!')
-            res.redirect('/user')
-        }catch(err){
             console.log(err)
         }
     }
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const userController = require('../controllers/user') 
 const { ensureAuth } = require('../middleware/auth')
 
-router.get('/', userController.getStatsAlt)
-router.post('/createStats', userController.createStats)
+router.get('/', ensureAuth, userController.getStats)
 
 module.exports = router
