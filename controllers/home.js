143133a1a8d8b889fcc9ad440f1891fa59988006
Kyle Christian <kylebolanos@gmail.com>
2022-09-23 15:48:43 -0700

    small bug fixes

--- controllers/home.js
+++ controllers/home.js
@@ -3,7 +3,7 @@ const Stat = require("../models/Stat");
 module.exports = {
   getIndex: async (req, res) => {
     try {
-      const stat = await Stat.find().sort({ time: "desc" }).lean();
+      const stat = await Stat.find().sort({ time: "desc" }).lean().limit(10);
       res.render("index.ejs", { stat: stat });
     } catch (err) {
       console.log(err);
